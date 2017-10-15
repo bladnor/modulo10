06ba748f0d16f0a48916b6221e2f79fcb44f4d81
Roland Berger <roland.berger@exasoft.ch>
2017-10-15 09:00:28 +0200

    rbe: gradleize

--- src/main/kotlin/Modulo10RecursivCheckdigitCalculator.kt
+++ src/main/kotlin/Modulo10RecursivCheckdigitCalculator.kt
@@ -1,5 +1,3 @@
-
-
 object Modulo10RecursivCheckdigitCalculator {
 
     /**
--- src/test/kotlin/Modulo10RecursivCheckdigitCalculatorTest.kt
+++ src/test/kotlin/Modulo10RecursivCheckdigitCalculatorTest.kt
@@ -1,8 +1,6 @@
-
-
-import ch.exasoft.easybilling.Modulo10RecursivCheckdigitCalculator.calculateCheckDigit
+import Modulo10RecursivCheckdigitCalculator.calculateCheckdigit
 import org.assertj.core.api.Assertions
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 
 class Modulo10RecursivCheckdigitCalculatorTest {
@@ -10,22 +8,25 @@ class Modulo10RecursivCheckdigitCalculatorTest {
 
     @Test
     fun `Assert that check digit is 8`() {
-        val checkDigit = calculateCheckDigit("70004152")
+        val checkDigit = calculateCheckdigit("70004152")
         Assertions.assertThat(checkDigit).isEqualTo(8)
     }
+
     @Test
     fun `Assert that check digit is 3`() {
-        val checkDigit = calculateCheckDigit("6451234567")
+        val checkDigit = calculateCheckdigit("6451234567")
         Assertions.assertThat(checkDigit).isEqualTo(3)
     }
+
     @Test
-    fun test3() {
-        val checkDigit = calculateCheckDigit("6541234567")
+    fun `Assert that check digit is 0`() {
+        val checkDigit = calculateCheckdigit("6541234567")
         Assertions.assertThat(checkDigit).isEqualTo(0)
     }
+
     @Test
-    fun test4() {
-        val checkDigit = calculateCheckDigit("5461234567")
+    fun `Assert that check digit is 2`() {
+        val checkDigit = calculateCheckdigit("5461234567")
         Assertions.assertThat(checkDigit).isEqualTo(2)
     }
 }
